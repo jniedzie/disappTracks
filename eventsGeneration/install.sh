98b6cdac8adb475fd57818ad680ec59825bfde69
jniedzie <jeremi.niedziela@cern.ch>
2019-02-22 15:16:18 +0100

    Updating install script

--- eventsGeneration/install.sh
+++ eventsGeneration/install.sh
@@ -3,19 +3,26 @@
 # add additional modules:
 echo "\nAdding additional CMSSW modules\n"
 
-#git cms-addpkg Configuration/StandardSequences
-#git cms-addpkg Configuration/DataProcessing
-#git cms-addpkg SimG4Core/CustomPhysics
+git cms-addpkg Configuration/StandardSequences
+git cms-addpkg Configuration/DataProcessing
+git cms-addpkg SimG4Core/CustomPhysics
 cd src
 
 echo "\nAdding chargino card and other necessary files\n"
 mkdir -p Configuration/GenProduction/python/ThirteenTeV/AMSB_chargino/
+mkdir -p DisappTrks/SignalMC/data/
+
 cd Configuration/GenProduction/python/ThirteenTeV/AMSB_chargino/
 wget https://raw.githubusercontent.com/jniedzie/disappTracks/master/eventsGeneration/AMSB_chargino300GeV_ctau10cm_NoFilter_13TeV.py
 
 cd ../..
 wget https://raw.githubusercontent.com/jniedzie/disappTracks/master/eventsGeneration/RandomSeed_cfi.py
 cd ../../..
+
+cd DisappTrks/SignalMC/data/
+wget https://github.com/jniedzie/disappTracks/raw/master/eventsGeneration/geant4cards.tar
+tar xvf geant4cards.tar
+rm geant4cards.tar
 
 echo "\nBuilding CMSSW\n"
 scram b -j8
