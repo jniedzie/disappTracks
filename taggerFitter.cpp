a23f3f3538e0527602e8acd2c4f6b6b2a84f30e6
Jeremi Niedziela <jeremi.niedziela@cern.ch>
2019-06-18 16:05:27 +0200

    Adding more options to taggerFitter and producing output file

--- taggerFitter.cpp
+++ taggerFitter.cpp
@@ -10,16 +10,19 @@
 #include "EventSet.hpp"
 
 string configPath = "../configs/helixTagger_maxHits.md";
+string outFilePrefix = "L2_4layers";
 string cutLevel = "after_L2/4layers/";//after_L1/";
 
-int nEvents = 20;
+int nEvents = 2;
 
 int nAnalyzedEvents = 0;
 
 xtracks::EDataType dataType = xtracks::kSignal;
 int setIter = kWino_M_300_cTau_10;
 
-shared_ptr<Event> GetEvent(int iEvent);
+string monitorType = "";
+string optimizationParam = "";
+
 vector<shared_ptr<Point>> GetClustersNoEndcaps(const shared_ptr<Event> &event, bool removePionClusters);
 TF1* GetRocFunction();
 
@@ -28,6 +31,8 @@ int    GetMaxNhits(vector<Helix> helices);
 int    GetMaxNlayers(vector<Helix> helices);
 double GetAvgLength(vector<Helix> helices);
 double GetMaxLength(vector<Helix> helices);
+
+void WriteOutputToFile(const TFitter *fitter, string outPath);
 
 void SetParameter(TFitter *fitter, int i,
                   string name, double start, double min, double max, double step,
@@ -63,9 +68,13 @@ void chi2Function(Int_t&, Double_t*, Double_t &f, Double_t *par, Int_t)
   config.mergeFinalHelices         = par[18];
   config.doAsymmetricConstraints   = par[19];
   config.allowTurningBack          = par[20];
+  config.candidateMinNpoints       = par[21];
   
   nAnalyzedEvents=0;
-  auto monitor = PerformanceMonitor("Max hits", 20, 0, 20 , nEvents);
+  int max = 20;
+  if(monitorType=="avg_length") max = 2;
+  if(monitorType=="max_length") max = 6;
+  auto monitor = PerformanceMonitor(monitorType, 20, 0, max, nEvents);
   
   for(auto iEvent=0; iEvent<nEvents; iEvent++){
     
@@ -78,25 +87,76 @@ void chi2Function(Int_t&, Double_t*, Double_t &f, Double_t *par, Int_t)
     for(auto &track : event->GetTracks()){
       vector<Helix> fittedHelicesSignal = helixFitter->FitHelices(pointsNoEndcapsSignal[iEvent], *track, *event->GetVertex());
       vector<Helix> fittedHelicesBackground = helixFitter->FitHelices(pointsNoEndcapsBackground[iEvent], *track, *event->GetVertex());
-      monitor.SetValues(iEvent, GetMaxNhits(fittedHelicesSignal), GetMaxNhits(fittedHelicesBackground));
+      
+      if(monitorType=="avg_hits"){
+        monitor.SetValues(iEvent, GetAvgNhits(fittedHelicesSignal), GetAvgNhits(fittedHelicesBackground));
+      }
+      else if(monitorType=="max_hits"){
+        monitor.SetValues(iEvent, GetMaxNhits(fittedHelicesSignal), GetMaxNhits(fittedHelicesBackground));
+      }
+      else if(monitorType=="max_layers"){
+        monitor.SetValues(iEvent, GetMaxNlayers(fittedHelicesSignal), GetMaxNlayers(fittedHelicesBackground));
+      }
+      else if(monitorType=="n_helices"){
+        monitor.SetValues(iEvent, fittedHelicesSignal.size(), fittedHelicesBackground.size());
+      }
+      else if(monitorType=="avg_length"){
+        monitor.SetValues(iEvent, GetAvgLength(fittedHelicesSignal), GetAvgLength(fittedHelicesBackground));
+      }
+      else if(monitorType=="max_length"){
+        monitor.SetValues(iEvent, GetMaxLength(fittedHelicesSignal), GetMaxLength(fittedHelicesBackground));
+      }
+      else{
+        cout<<"Unknown monitor type: "<<monitorType<<endl;
+        cout<<"Possible options: avg_hits/max_hits/max_layers/n_helices/avg_length/max_length"<<endl;
+        exit(0);
+      }
+
     }
     nAnalyzedEvents++;
   }
   monitor.CalcEfficiency(nAnalyzedEvents);
-  //    double auc = monitor.GetAUC();
-  double significance = monitor.GetSignificanceAfterL0();
-  double chi2 = pow(20-significance, 2)/significance;
   
-  cout<<"chi2: "<<chi2<<"\tsignificance: "<<significance<<endl;
+  double optValue = inf;
+  double chi2 = inf;
+  
+  if(optimizationParam=="auc"){
+    optValue = monitor.GetAUC();
+    chi2 = pow(1-optValue, 2)/optValue;
+  }
+  else if(optimizationParam=="max_eff"){
+    optValue = monitor.GetMaxEfficiency();
+    chi2 = pow(1-optValue, 2)/optValue;
+  }
+  else if(optimizationParam=="sigma_init"){
+    optValue = monitor.GetSignificanceInitial();
+    chi2 = pow(20-optValue, 2)/optValue;
+  }
+  else if(optimizationParam=="sigma_L0"){
+    optValue = monitor.GetSignificanceAfterL0();
+    chi2 = pow(20-optValue, 2)/optValue;
+  }
+  else{
+    cout<<"Unknown optimization param: "<<optimizationParam<<endl;
+    cout<<"Possible options: auc/max_eff/sigma_init/sigma_L0"<<endl;
+    exit(0);
+  }
+
+  cout<<"chi2: "<<chi2<<"\t"<<optimizationParam<<": "<<optValue<<endl;
   f = chi2;
 };
 
 int main(int argc, char* argv[])
 {
+  if(argc!=3){
+    cout<<"Usage: ./taggerFitter [avg_hits/max_hits/max_layers/n_helices/avg_length/max_length] [auc/max_eff/sigma_init/sigma_L0]"<<endl;
+    exit(0);
+  }
+  monitorType = argv[1];
+  optimizationParam = argv[2];
+  
   config = ConfigManager(configPath);
-  
   EventSet eventsSet;
-  
   
   for(auto iEvent=0; iEvent<nEvents; iEvent++){
     eventsSet.LoadEventFromFiles(dataType, setIter, iEvent, cutLevel);
@@ -111,7 +171,7 @@ int main(int argc, char* argv[])
   }
   
 
-  const int nPar = 21;
+  const int nPar = 22;
   TVirtualFitter::SetDefaultFitter("Minuit");
   TFitter *fitter = new TFitter(nPar);
   fitter->SetFCN(chi2Function);
@@ -135,13 +195,13 @@ int main(int argc, char* argv[])
   SetParameter(fitter, 12, "next_point_max_delta_xy"       ,  20  ,  0.0  , 300 , 10 , fix);
   SetParameter(fitter, 13, "track_min_n_points"            ,  3   ,  3    , 20  , 1  , fix);
   SetParameter(fitter, 14, "merging_max_different_point"   ,  2   ,  0    , 20  , 1  , fix);
-  
   SetParameter(fitter, 15, "max_n_missing_hits"            ,  1   ,  0    , 5   , 1  , fix);
   SetParameter(fitter, 16, "max_n_missing_hits_in_raw"     ,  1   ,  0    , 5   , 1  , fix);
   SetParameter(fitter, 17, "merge_at_turn_back"            ,  0   ,  0    , 2   , 1  , fix);
   SetParameter(fitter, 18, "merge_final_helices"           ,  0   ,  0    , 2   , 1  , fix);
   SetParameter(fitter, 19, "do_asymmetric_constraints"     ,  1   ,  0    , 2   , 1  , fix);
   SetParameter(fitter, 20, "allow_turning_back"            ,  1   ,  0    , 2   , 1  , fix);
+  SetParameter(fitter, 21, "candidate_min_n_points"        ,  3   ,  3    , 10  , 1  , fix);
   
   double args = 0; // put to 0 for results only, or to -1 for no garbage
   fitter->ExecuteCommand( "SET PRINTOUT"  , &args, 1);
@@ -151,6 +211,10 @@ int main(int argc, char* argv[])
   double arglist[1] = {0};
   fitter->ExecuteCommand("MIGRAD", arglist, 0);
 
+  string outPath = "taggerFitterOutput_"+outFilePrefix+"_"+monitorType+"_"+optimizationParam+".txt";
+  
+  WriteOutputToFile(fitter, outPath);
+  
   return 0;
 }
 
@@ -241,4 +305,46 @@ double GetMaxLength(vector<Helix> helices)
     if(length > maxLength) maxLength = length;
   }
   return maxLength;
+}
+
+void WriteOutputToFile(const TFitter *fitter, string outPath)
+{
+  ofstream outFile(outPath);
+  outFile << "Output for data: " << cutLevel << "\t monitor: "<<monitorType<<"\toptimizing: "<<optimizationParam<<endl;
+  
+  outFile << endl;
+  outFile << "double_hit_max_distance: "        << fitter->GetParameter(0)  << endl;
+  outFile << endl;
+  outFile << "seed_max_chi2: "                  << fitter->GetParameter(1)  << endl;
+  outFile << endl;
+  outFile << "seed_middle_hit_min_delta_phi: "  << fitter->GetParameter(2)  << endl;
+  outFile << "seed_middle_hit_max_delta_phi: "  << fitter->GetParameter(3)  << endl;
+  outFile << "seed_middle_hit_max_delta_z: "    << fitter->GetParameter(4)  << endl;
+  outFile << endl;
+  outFile << "seed_last_hit_min_delta_phi: "    << fitter->GetParameter(5)  << endl;
+  outFile << "seed_last_hit_max_delta_phi: "    << fitter->GetParameter(6)  << endl;
+  outFile << "seed_last_hit_max_delta_z: "      << fitter->GetParameter(7)  << endl;
+  outFile << endl;
+  outFile << "track_max_chi2: "                 << fitter->GetParameter(8)  << endl;
+  outFile << endl;
+  outFile << "next_point_min_delta_phi: "       << fitter->GetParameter(9)  << endl;
+  outFile << "next_point_max_delta_phi: "       << fitter->GetParameter(10) << endl;
+  outFile << "next_point_max_delta_z: "         << fitter->GetParameter(11) << endl;
+  outFile << "next_point_max_delta_xy: "        << fitter->GetParameter(12) << endl;
+  outFile << endl;
+  outFile << "track_min_n_points: "             << fitter->GetParameter(13) << endl;
+  outFile << endl;
+  outFile << "merging_max_different_point: "    << fitter->GetParameter(14) << endl;
+  outFile << "candidate_min_n_points: "         << fitter->GetParameter(21) << endl;
+  outFile << "merge_at_turn_back: "             << fitter->GetParameter(17) << endl;
+  outFile << "merge_final_helices: "            << fitter->GetParameter(18) << endl;
+  
+  outFile << "max_n_missing_hits: "             << fitter->GetParameter(15) << endl;
+  outFile << "max_n_missing_hits_in_raw: "      << fitter->GetParameter(16) << endl;
+  outFile << endl;
+  outFile << "do_asymmetric_constraints: "      << fitter->GetParameter(19) << endl;
+  outFile << endl;
+  outFile << "allow_turning_back: "             << fitter->GetParameter(20) << endl;
+  
+  outFile.close();
 }
