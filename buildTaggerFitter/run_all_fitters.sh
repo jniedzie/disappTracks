b7b30107fde18dcd6a1d77c72607733eef1c5374
Jeremi Niedziela <jeremi.niedziela@cern.ch>
2019-06-19 10:45:05 +0200

    Adding separate condor configs for each monitor-optimizer pair and a script to submit them all

---
+++ buildTaggerFitter/run_all_fitters.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+condor_submit condorConfig_maxHits_auc.sub
+condor_submit condorConfig_maxHits_maxEff.sub
+condor_submit condorConfig_maxHits_sigmaInit.sub
+condor_submit condorConfig_maxHits_sigmaL0.sub
+
+condor_submit condorConfig_maxLayers_auc.sub
+condor_submit condorConfig_maxLayers_maxEff.sub
+condor_submit condorConfig_maxLayers_sigmaInit.sub
+condor_submit condorConfig_maxLayers_sigmaL0.sub
+
+condor_submit condorConfig_maxLength_auc.sub
+condor_submit condorConfig_maxLength_maxEff.sub
+condor_submit condorConfig_maxLength_sigmaInit.sub
+condor_submit condorConfig_maxLength_sigmaL0.sub
+
+condor_submit condorConfig_nHelices_auc.sub
+condor_submit condorConfig_nHelices_maxEff.sub
+condor_submit condorConfig_nHelices_sigmaInit.sub
+condor_submit condorConfig_nHelices_sigmaL0.sub
