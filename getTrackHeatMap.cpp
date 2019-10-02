57badb208d3cd7852ac893dd574ef36cde531a24
jniedzie <jeremi.niedziela@cern.ch>
2019-10-02 09:48:16 +0200

    Adding limits results for all combinations of mass-ct-category

---
+++ getTrackHeatMap.cpp
@@ -0,0 +1,9 @@
+//
+//  getTrackHeatMap.cpp
+//  getTrackHeatMap
+//
+//  Created by Jeremi Niedziela on 01/10/2019.
+//  Copyright © 2019 Jeremi Niedziela. All rights reserved.
+//
+
+#include <stdio.h>
