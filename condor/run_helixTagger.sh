6642f6b2c769a890484fd7776f56361c64d8689f
Jeremi Niedziela <jeremi.niedziela@cern.ch>
2019-10-18 14:02:17 +0200

    Another fix for compilation on lxplus

--- condor/run_helixTagger.sh
+++ condor/run_helixTagger.sh
@@ -2,20 +2,8 @@
 
 echo "Starting helix tagger"
 
-#export lcgenv=/cvmfs/sft.cern.ch/lcg/releases/lcgenv/0.2-bbd0d/x86_64-centos7-gcc48-dbg/lcgenv
-
-# setup GCC
-#source /cvmfs/sft.cern.ch/lcg/contrib/gcc/4.8/x86_64-centos7-gcc48-opt/setup.sh
-. /cvmfs/sft.cern.ch/lcg/contrib/gcc/8/x86_64-centos7/setup.sh
-
-# setup PCRE
-source /cvmfs/sft.cern.ch/lcg/releases/pcre/8.34-2c9d9/x86_64-centos7-gcc49-opt/pcre-env.sh
-
-# setup ROOT
-#source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.16.00/x86_64-centos7-gcc48-opt/bin/thisroot.sh
-. /cvmfs/sft.cern.ch/lcg/releases/LCG_96/ROOT/6.18.00/x86_64-centos7-gcc8-opt/bin/thisroot.sh
-
 cd /afs/cern.ch/work/j/jniedzie/private/disapp_tracks/disappTracks/
+. setenv.sh
 
 echo "Im in `pwd`"
-./helixTagger $1 $2
+./helixTagger afterHelixTagging_chunk$1 $1 1
--- setenv.sh
+++ setenv.sh
@@ -2,9 +2,27 @@
 
 alias cmake='/cvmfs/sft.cern.ch/lcg/contrib/CMake/3.13.4/Linux-x86_64/bin/cmake'
 
-#. /cvmfs/sft.cern.ch/lcg/contrib/gcc/4.8/x86_64-centos7-gcc48-opt/setup.sh
-#. /cvmfs/sft.cern.ch/lcg/contrib/gcc/6.4/x86_64-centos7/setup.sh
+# setup LCG env
+#export lcgenv=/cvmfs/sft.cern.ch/lcg/releases/lcgenv/1.3.6-e63db/x86_64-centos7-gcc8-opt/lcgenv
+
+# setup VDT
+. /cvmfs/sft.cern.ch/lcg/releases/vdt/0.4.3-992df/x86_64-centos7-gcc8-opt/vdt-env.sh
+
+# setup GCC
+#. /cvmfs/sft.cern.ch/lcg/contrib/gcc/6.2/x86_64-centos7/setup.sh
+#. /cvmfs/sft.cern.ch/lcg/contrib/gcc/7/x86_64-centos7/setup.sh
 . /cvmfs/sft.cern.ch/lcg/contrib/gcc/8/x86_64-centos7/setup.sh
 
-#. /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.16.00/x86_64-centos7-gcc48-opt/bin/thisroot.sh
+# setup PCRE
+#. /cvmfs/sft.cern.ch/lcg/releases/pcre/8.43-511cb/x86_64-centos7-gcc8-opt/pcre-env.sh
+
+#setup TBB
+. /cvmfs/sft.cern.ch/lcg/releases/tbb/2019_U7-ba3eb/x86_64-centos7-gcc8-opt/tbb-env.sh
+
+# setup Davix
+. /cvmfs/sft.cern.ch/lcg/releases/Davix/0.7.3-d94fa/x86_64-centos7-gcc8-opt/Davix-env.sh
+
+# setup ROOT
+#. /cvmfs/sft.cern.ch/lcg/releases/LCG_89/ROOT/6.10.02/x86_64-centos7-gcc7-opt/bin/thisroot.sh
+#. /cvmfs/sft.cern.ch/lcg/releases/LCG_91/ROOT/6.10.06/x86_64-centos7-gcc7-opt/bin/thisroot.sh
 . /cvmfs/sft.cern.ch/lcg/releases/LCG_96/ROOT/6.18.00/x86_64-centos7-gcc8-opt/bin/thisroot.sh
