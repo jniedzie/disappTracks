45e581dd53ecd88c4cfa4dadd024e580e84fb634
Jeremi Niedziela <jeremi.niedziela@cern.ch>
2019-12-03 11:12:22 +0100

    Updating scripts to run helix tagger with condor

--- condor/run_helixTagger.sh
+++ condor/run_helixTagger.sh
@@ -5,69 +5,55 @@ echo "Starting helix tagger"
 cd /afs/cern.ch/work/j/jniedzie/private/disapp_tracks/disappTracks/
 . setenv.sh
 
-configPath="configs/helixTagger.md"
-outputPath="afterHelixTagging"
+configPath=""
+outputPath=""
 
 if [ $2 -eq 0 ];
 then
   configPath="configs/helixTagger.md"
-  outputPath="after_L1/all/afterHelixTagging_noHighPtHits"
+  outputPath="after_L1/all/afterHelixTagging_default"
 elif [ $2 -eq 1 ];
 then
-  configPath="configs/helixTaggerWithEndcaps.md"
-  outputPath="afterHelixTagging_withEndcaps"
+  configPath="configs/helixTaggerBkg.md"
+  outputPath="after_L0/afterHelixTagging_default"
 elif [ $2 -eq 2 ];
 then
-  configPath="configs/helixTaggerAsymmetric.md"
-  outputPath="afterHelixTagging_asymmetric"
+  configPath="configs/helixTaggerHighMerging.md"
+  outputPath="after_L1/all/afterHelixTagging_highMerging"
 elif [ $2 -eq 3 ];
 then
-  configPath="configs/helixTaggerHighMerging.md"
-  outputPath="afterHelixTagging_highMerging"
+  configPath="configs/helixTaggerHighMergingBkg.md"
+  outputPath="after_L0/afterHelixTagging_highMerging"
 elif [ $2 -eq 4 ];
 then
   configPath="configs/helixTaggerLowSeedChi.md"
-  outputPath="afterHelixTagging_lowSeedChi"
+  outputPath="after_L1/all/afterHelixTagging_lowSeedChi"
 elif [ $2 -eq 5 ];
 then
-  configPath="configs/helixTaggerLowTrackChi.md"
-  outputPath="afterHelixTagging_lowTrackChi"
+  configPath="configs/helixTaggerLowSeedChiBkg.md"
+  outputPath="after_L0/afterHelixTagging_lowSeedChi"
 elif [ $2 -eq 6 ];
 then
-  configPath="configs/helixTaggerNoMissing.md"
-  outputPath="afterHelixTagging_noMissing"
+  configPath="configs/helixTaggerLowTrackChi.md"
+  outputPath="after_L1/all/afterHelixTagging_lowTrackChi"
 elif [ $2 -eq 7 ];
 then
-  configPath="configs/helixTaggerNoNoise.md"
-  outputPath="afterHelixTagging_noNoise"
+  configPath="configs/helixTaggerLowTrackChiBkg.md"
+  outputPath="after_L0/afterHelixTagging_lowTrackChi"
 elif [ $2 -eq 8 ];
 then
-  configPath="configs/helixTaggerNoNoiseBkg.md"
-  outputPath="afterHelixTagging_noNoise"
+  configPath="configs/helixTaggerNoMissing.md"
+  outputPath="after_L1/all/afterHelixTagging_noMissing"
 elif [ $2 -eq 9 ];
 then
-  configPath="configs/helixTaggerLowSeedChiBkg.md"
-  outputPath="afterHelixTagging_lowSeedChi"
+  configPath="configs/helixTaggerNoMissingBkg.md"
+  outputPath="after_L0/afterHelixTagging_noMissing"
 elif [ $2 -eq 10 ];
 then
-  configPath="configs/helixTaggerLowTrackChiBkg.md"
-  outputPath="afterHelixTagging_lowTrackChi"
-elif [ $2 -eq 11 ];
-then
-  configPath="configs/helixTagger3layers.md"
-  outputPath="after_L1/3-layers/afterHelixTagging"
-elif [ $2 -eq 12 ];
-then
-  configPath="configs/helixTagger4layers.md"
-  outputPath="after_L1/4-layers/afterHelixTagging"
-elif [ $2 -eq 13 ];
-then
-  configPath="configs/helixTagger5-6layers.md"
-  outputPath="after_L1/5-6-layers/afterHelixTagging"
-elif [ $2 -eq 14 ];
-then
-  configPath="configs/helixTaggerBkg.md"
-  outputPath="after_L0/afterHelixTagging"
+  configPath="configs/helixTaggerNoPionHits.md"
+  outputPath="after_L1/all/afterHelixTagging_noPionHits"
+else
+  echo "Unkown option!!"
 fi
 
 echo "Im in `pwd`"
